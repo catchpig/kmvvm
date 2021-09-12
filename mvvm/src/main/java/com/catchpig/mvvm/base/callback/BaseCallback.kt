c219c5af72046f5ddc9809ba09395c081f52c587
tli2 <Tao.Li2@harman.com>
2021-09-12 17:46:20 +0800

    解决BaseCallback在onError的时候没有把loading画面关掉

--- mvvm/src/main/java/com/catchpig/mvvm/base/callback/BaseCallback.kt
+++ mvvm/src/main/java/com/catchpig/mvvm/base/callback/BaseCallback.kt
@@ -1,6 +1,7 @@
 package com.catchpig.mvvm.base.callback
 
 import com.catchpig.mvvm.base.viewmodel.BaseViewModel
+import com.catchpig.utils.ext.loge
 import io.reactivex.rxjava3.subscribers.ResourceSubscriber
 
 /**
@@ -8,8 +9,15 @@ import io.reactivex.rxjava3.subscribers.ResourceSubscriber
  * date 2021/9/11
  * description:
  */
-abstract class BaseCallback<T>(var baseViewModel: BaseViewModel? = null, var isLoadingDialog: Boolean = true) :
+abstract class BaseCallback<T>(
+    var baseViewModel: BaseViewModel? = null,
+    var isLoadingDialog: Boolean = true
+) :
     ResourceSubscriber<T>() {
+    companion object {
+        private const val TAG = "BaseCallback"
+    }
+
     override fun onStart() {
         super.onStart()
         baseViewModel?.let {
@@ -24,10 +32,19 @@ abstract class BaseCallback<T>(var baseViewModel: BaseViewModel? = null, var isL
     abstract fun onSuccess(t: T)
 
     override fun onError(t: Throwable?) {
-
+        t?.let { it ->
+            it.message?.let {
+                it.loge(TAG)
+            }
+        }
+        hideLoading()
     }
 
     override fun onComplete() {
+        hideLoading()
+    }
+
+    private fun hideLoading() {
         baseViewModel?.let {
             it.hideLoading()
         }
