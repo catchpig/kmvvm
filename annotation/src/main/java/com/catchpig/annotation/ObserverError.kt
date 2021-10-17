6892051cfc32a1766436a4a9317a36a9e1ca8756
catch-pig <15882062220>
2021-10-17 11:10:07 +0800

    调整maven地址顺序

--- annotation/src/main/java/com/catchpig/annotation/ObserverError.kt
+++ annotation/src/main/java/com/catchpig/annotation/ObserverError.kt
@@ -1,12 +1,10 @@
 package com.catchpig.annotation
 
 /**
- * SharedPreferences注解,可生成对应的文件
+ * ViewModel中OnError的统一处理
  * @author catchpig
  * @date 2019/10/29 00:29
  */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
-annotation class ObserverError(
-
-)
+annotation class ObserverError
