fb124062d20f9678d6dc430eb0a5939f5a341e8f
catch-pig <327785185@qq.com>
2024-11-13 21:54:30 +0800

    升级material版本,解决高版本反射崩溃问题

--- utils/src/main/java/com/catchpig/utils/ext/SnackbarExt.kt
+++ utils/src/main/java/com/catchpig/utils/ext/SnackbarExt.kt
@@ -1,8 +1,8 @@
 package com.catchpig.utils.ext
 
-import android.graphics.Rect
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.annotation.ColorRes
@@ -13,17 +13,20 @@ import androidx.core.view.updateLayoutParams
 import com.catchpig.utils.R
 import com.google.android.material.snackbar.Snackbar
 
-fun View.showSnackBar(text: CharSequence, @DrawableRes background: Int, gravity: Int = Gravity.BOTTOM) {
+fun View.showSnackBar(
+    text: CharSequence,
+    @DrawableRes background: Int,
+    gravity: Int = Gravity.BOTTOM
+) {
     val snackbar = Snackbar.make(this, text, Snackbar.LENGTH_LONG)
     snackbar.setBackgroundResource(background)
     snackbar.setTextColorRes(R.color.color_black)
     if (gravity == Gravity.BOTTOM) {
-        val parentView = snackbar.targetParent()
-        val bottom = 20 + context.px2dp((parentView.bottom - bottom))
-        snackbar.setMargin(20, 0, 20, bottom)
+        val layoutParams = snackbar.view.layoutParams as MarginLayoutParams
+        layoutParams.bottomMargin = dp2px(20)
+        snackbar.view.layoutParams = layoutParams
     } else {
         snackbar.setGravity(gravity)
-        snackbar.setMargin(20, 0, 20, 0)
     }
     snackbar.show()
 }
@@ -37,12 +40,11 @@ fun View.showSnackBar(
     snackbar.setBackgroundResource(background)
     snackbar.setTextColorRes(R.color.color_black)
     if (gravity == Gravity.BOTTOM) {
-        val parentView = snackbar.targetParent()
-        val bottom = 20 + context.px2dp((parentView.bottom - bottom))
-        snackbar.setMargin(20, 0, 20, bottom)
+        val layoutParams = snackbar.view.layoutParams as MarginLayoutParams
+        layoutParams.bottomMargin = dp2px(20)
+        snackbar.view.layoutParams = layoutParams
     } else {
         snackbar.setGravity(gravity)
-        snackbar.setMargin(20, 0, 20, 0)
     }
     snackbar.show()
 }
@@ -73,30 +75,10 @@ fun Snackbar.setGravity(gravity: Int) {
             is FrameLayout.LayoutParams -> {
                 this.gravity = gravity
             }
+
             is CoordinatorLayout.LayoutParams -> {
                 this.gravity = gravity
             }
         }
     }
-}
-
-fun Snackbar.targetParent(): View {
-    val cls = this::class.java.superclass
-    val field = cls.getDeclaredField("targetParent")
-    field.isAccessible = true
-    return field.get(this) as View
-}
-
-/**
- * 设置外边框
- */
-fun Snackbar.setMargin(startDp: Int, topDp: Int, endDp: Int, bottomDp: Int) {
-    val cls = this::class.java.superclass
-    val field = cls.getDeclaredField("originalMargins")
-    field.isAccessible = true
-    val marginStart = context.dp2px(startDp)
-    val topMargin = context.dp2px(topDp)
-    val marginEnd = context.dp2px(endDp)
-    val bottomMargin = context.dp2px(bottomDp)
-    field.set(this, Rect(marginStart, topMargin, marginEnd, bottomMargin))
 }
