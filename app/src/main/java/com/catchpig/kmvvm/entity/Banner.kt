566400369d57e4dd02e05634e5704af526e5993f
Administrator <327785185@qq.com>
2025-08-09 09:00:51 +0800

    升级gradle版本

--- app/src/main/java/com/catchpig/kmvvm/entity/Banner.kt
+++ app/src/main/java/com/catchpig/kmvvm/entity/Banner.kt
@@ -1,6 +1,8 @@
 package com.catchpig.kmvvm.entity
 
+import android.annotation.SuppressLint
 import kotlinx.serialization.Serializable
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class Banner(val desc: String, val imagePath: String, val title: String)
